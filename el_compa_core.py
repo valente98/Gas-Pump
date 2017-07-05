0a88688035acef0cae8ea075d9d4d3db84de4a7c
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-05 11:08:02 -0500

    deleted some funtions and created another incompleted function.

--- el_compa_core.py
+++ el_compa_core.py
@@ -1,18 +1,12 @@
-def gas_price():
+def gas_price(gas, amount):
     """ str -> Float"""
     if gas == '1':
-        amount *= 1.99
+        return float(amount) * 1.99
     elif gas == '2':
-        amount *= 2.37
+        return float(amount) * 2.37
     elif gas == '3':
-        amount *= 2.99
-    return amount
-
-def pay_before():
+        return float(amount) * 2.99
+def convert_gallons():
     return None
-
-def pay_after():
-    return None
-
 def keep_log():
     return None
