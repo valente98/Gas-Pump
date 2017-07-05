cce96e0b227def8191b807cc9a3dad728e2bc3f6
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-05 12:00:57 -0500

    completed the keep log function

--- el_compa_core.py
+++ el_compa_core.py
@@ -6,8 +6,21 @@ def gas_price(gas, amount):
         return float(amount) * 2.37
     elif gas == '3':
         return float(amount) * 2.99
-        
-def keep_log():
-    with open('log.txt', 'a') as file:
 
+def keep_log(gas,amount):
+    R = 'Regular'
+    M = 'Midgrade'
+    P = 'Premium'
+
+    price = gas_price(gas,amount)
+
+    message = ''
+    if gas == '1':
+            message = '{}, {}, {}'.format(R, amount, price)
+    elif gas == '2':
+        message = '{}, {}, {}'.format(M, amount, price)
+    elif gas == '3':
+            message = '{}, {}, {}'.format(P, amount, price)
+    with open('log.txt', 'a') as file:
+        file.write(message)
     return None
