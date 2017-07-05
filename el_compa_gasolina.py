5688c48a0aa6ef82f9de4396898a666af885949b
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-05 10:01:27 -0500

    created an unfinished main fucntion that will be used

---
+++ el_compa_gasolina.py
@@ -0,0 +1,16 @@
+import el_compa_core
+def main():
+    msg = '''Hello! welcome to El Compa Gasolina. Wich type of gass would you like?
+    \t1. Regular $1.99\n
+    \t2. Midgrade $2.37\n
+    \t3. Premium $2.99\n'''
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
