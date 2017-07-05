434402a4a50f529e6f852056d94a42fb34229764
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-05 15:29:21 -0500

    modified the function where it will be short and simple

--- el_compa_gasolina.py
+++ el_compa_gasolina.py
@@ -1,28 +1,22 @@
 import el_compa_core
+
 def main():
     msg = ''' Hello! welcome to El Compa Gasolina. Wich type of gass would you like?
     \t1. Regular $1.99\n
     \t2. Midgrade $2.37\n
     \t3. Premium $2.99\n '''
-    while '1' or '2' or '3':
+    while True:
         gas = input(msg)
-        if gas == '1':
-            amount = input('How many gallons would you like? ')
-            el_compa_core.keep_log(gas,amount)
-            print('Your total will be ${:.2f}'.format(el_compa_core.gas_price(gas, amount)))
-            break
-        elif gas == '2':
-            amount = input('How many gallons would you like? ')
-            el_compa_core.keep_log(gas,amount)
-            print('Your total will be ${:.2f}'.format(el_compa_core.gas_price(gas, amount)))
-            break
-        elif gas == '3':
-            amount = input('How many gallons would you like? ')
-            el_compa_core.keep_log(gas,amount)
-            print('Your total will be ${:.2f}'.format(el_compa_core.gas_price(gas, amount)))
+        if gas == '1' or gas == '2' or gas == '3':
             break
         else:
             print('Sorry, invalid choice!')
+    
+    amount = input('How many gallons would you like? ')
+    print('Your total will be ${:.2f}'.format(el_compa_core.gas_price(gas, amount)))
+
+    gas_type = el_compa_core.keep_log(gas, amount)
+    el_compa_core.take_away(gas_type, amount)
 
     print('Thank you for shopping with us today! Have a nice day!!')
 
