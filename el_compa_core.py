9991ee07f80f5a598b111a5e8b5839cc6d4de027
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-05 11:24:43 -0500

    completed the convert_gallons function

--- el_compa_core.py
+++ el_compa_core.py
@@ -7,6 +7,12 @@ def gas_price(gas, amount):
     elif gas == '3':
         return float(amount) * 2.99
 def convert_gallons():
-    return None
+    """ str -> Float """
+    if gas == '1':
+        return float(amount) / 1.99
+    elif gas == '2':
+        return float(amount) / 2.37
+    elif gas == '3':
+        return float(amount) / 2.99
 def keep_log():
     return None
