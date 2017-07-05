7872e71ee877928d81fd820fb1b09d5445a77721
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-05 10:48:25 -0500

    added some modifications to the function

--- el_compa_gasolina.py
+++ el_compa_gasolina.py
@@ -1,16 +1,26 @@
 import el_compa_core
 def main():
-    msg = '''Hello! welcome to El Compa Gasolina. Wich type of gass would you like?
+    msg = ''' Hello! welcome to El Compa Gasolina. Wich type of gass would you like?
     \t1. Regular $1.99\n
     \t2. Midgrade $2.37\n
-    \t3. Premium $2.99\n'''
+    \t3. Premium $2.99\n '''
     
+    gas = input(msg)
+
+    if gas == '1':
+        amount = input('How much gas would you like?')
+        print('Your total will be $' + el_compa_core.gas_price(gas, amount))
+    elif gas == '2':
+        amount = input('How much gas would you like?')
+        print('Your total will be $' + el_compa_core.gas_price(gas, amount))
+    elif gas == '3':
+        amount = input('How much gas would you like?')
+        print('Your total will be $' + el_compa_core.gas_price(gas, amount))
+    else:
+        print('Sorry, invalid choice!')
 
 
+    if __name__ == '__main__':
+        main()
 
-
-
-
-if __name__ == '__main__':
-
-
+        
