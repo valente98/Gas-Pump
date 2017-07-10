46ef87f8cdfa5dab38f70dd98b51abe01ef8b219
valente alvarez <valvarez@basecampcodingacademy.org>
2017-07-10 13:55:58 -0500

    added an if statemet to make sure that the user would not type the number or have a space in between

--- el_compa_gasolina.py
+++ el_compa_gasolina.py
@@ -1,7 +1,9 @@
 import el_compa_core
 
 def main():
-    msg = ''' Hello! welcome to El Compa Gasolina. Wich type of gass would you like?
+    print('Hello! welcome to El Compa Gasolina.')
+   
+    msg = '''  Wich type of gass would you like?
     \t1. Regular $1.99\n
     \t2. Midgrade $2.37\n
     \t3. Premium $2.99\n '''
@@ -14,19 +16,23 @@ def main():
         if gas.lower() == 'revenue':
             print('your total revenue is ${:.2f}'.format(el_compa_core.revenue_log()))
             return None
-        if gas == '1' or gas == '2' or gas == '3':
+        if gas == '1' or gas == '2' or gas == '3' or gas.lower()== 'one' or gas.lower() == 'two' or gas.lower() == 'three':
             break
         else:
             print('Sorry, invalid choice!')
     amount = input('How many gallons would you like? ')
+    if not amount.strip().isnumeric():
+        print('Sorry, invalid choice.')
+        return None
+
+    gas_type = el_compa_core.get_gas_type(gas)
+    if not el_compa_core.take_away(gas_type, amount):
+        print('Please come back again!')
+        return None
+
     print('Your total will be ${:.2f}'.format(el_compa_core.gas_price(gas, amount)))
-
-    gas_type = el_compa_core.keep_log(gas, amount)
-    el_compa_core.take_away(gas_type, amount)
+    el_compa_core.keep_log(gas, amount, gas_type)
     print('Thank you for shopping with us today! Have a nice day!!')
-
 
 if __name__ == '__main__':
     main()
-
-        
